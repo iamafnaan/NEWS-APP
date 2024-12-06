416699bf83926786dc0e8c7fd7e045eccde1c9a0
iamafnaan <afnaanahmed33@gmail.com>
2024-12-06 21:01:02 +0530

    struct change in the newsApi file

--- server/config/newsApi.js
+++ server/config/newsApi.js
@@ -2,6 +2,6 @@
 export const BASE_URL = 'https://newsapi.org/v2';
 export const SOURCES = {
   INDIA: 'google-news-in',
-  // US: 'google-news'
+  
 };
 export const NEWS_API_KEY = process.env.NEWS_API_KEY;
